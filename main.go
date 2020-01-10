c58cabf8892d17f29613026d65cc6a672652a9c6
Zero101010 <igotaraujo97@gmail.com>
2020-01-09 22:08:07 -0300

    Adicionando endpoints com dados estáticos de um array

--- main.go
+++ main.go
@@ -1,14 +1,74 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+// Struct de dados da pessoa
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+
+// Struct de dados de Adress
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty`
+}
+
+var people []Person
+
+// Retorna todas as pessoas que foram adicionadas
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+}
+
+// Pega somente a pessoa com o id passado como parametro
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+
+// cria uma nova pessoa e insere no array
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
+
+// deleta uma pessoa do array
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
+}
+
 // função principal
 func main() {
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+
 	router := mux.NewRouter()
 	router.HandleFunc("/contato", GetPeople).Methods("GET")
 	router.HandleFunc("/contato/{id}", GetPerson).Methods("GET")
